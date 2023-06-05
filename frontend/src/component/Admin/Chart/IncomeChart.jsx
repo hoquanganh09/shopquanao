855fdcd92ca09fd38209ecb1c3a48b3998d70d4c
hoquanganh09 <76956723+hoquanganh09@users.noreply.github.com>
2023-06-05 12:09:57 +0700

    Update Chart

--- frontend/src/component/Admin/Chart/IncomeChart.jsx
+++ frontend/src/component/Admin/Chart/IncomeChart.jsx
@@ -6,7 +6,7 @@ const IncomeChart = () => {
     <h1 style={{textAlign:"center", padding:"1vh"}}>Biểu đồ Thu nhập từng tháng</h1>
   <iframe 
   style={{width:"100%", height:"500px"}}
-  src=""
+  src="https://charts.mongodb.com/charts-demo_shopquanao-ypvcz/embed/charts?id=647d6d72-a88f-4d98-849c-011d4f112162&maxDataAge=3600&theme=light&autoRefresh=true"
   title="YouTube video"
 ></iframe>
 </div>
